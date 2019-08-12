deab33b6d62d2f53db52308f265a35d15d7f7976
ivan.korendiasev <idk97@mail.com>
2019-08-12 13:17:37 +0300

    App is created

---
+++ SearchApp/SearchApp/SearchApp/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace SearchApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 423);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(773, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(480, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(480, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(308, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(480, 188);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(308, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(692, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Пауза";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(480, 216);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(308, 40);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Сброс настроек";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(477, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Название файла";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(477, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Текст в файле:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(477, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Стартовая директория:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(590, 262);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 34);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Стоп";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(477, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Прошло времени:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(477, 330);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Проверено файлов:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 388);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Сейчас проверяется:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(480, 124);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(161, 28);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Выбрать директорию";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(452, 340);
+            this.listBox1.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Найденные файлы:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(700, 29);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(88, 22);
+            this.textBox4.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(674, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 29);
+            this.label8.TabIndex = 18;
+            this.label8.Text = ".";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(697, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 17);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Расширение";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(179, 388);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 17);
+            this.label10.TabIndex = 20;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(614, 299);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 17);
+            this.label11.TabIndex = 21;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(622, 330);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 17);
+            this.label12.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 460);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "SearchApp";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ SearchApp/SearchApp/SearchApp/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace SearchApp
+{
+    public partial class Form1 : Form
+    {
+        string directory;
+        
+        Thread searching;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            searching = new Thread(Search);
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            FBD.ShowDialog();
+            directory = FBD.SelectedPath;
+            textBox3.Text = directory;
+            button1.Enabled = true;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (searching.IsAlive)
+            {
+                searching.Resume();
+            }
+            else
+            {
+                searching = new Thread(Search);
+                searching.Start();
+            }
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button4.Enabled = true;
+
+
+        }
+
+        public void Search()
+        {
+            int progress;
+            TimeSpan ts;
+            DateTime t1, t2;
+            string time;
+            t1 = DateTime.Now;
+            int count = 0;
+            string filename;
+            string nametype = "*";
+            
+            if ((textBox4.Text != ""))
+            {
+                nametype = "*." + textBox4.Text;
+            }
+            string[] files = Directory.GetFiles(directory, nametype, SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                count++;
+                bool matchtext = false;
+                bool matchname = false;
+                bool hasnamecriteria = false;
+                if (textBox1.Text != "") //проверка отсутствия критериев текста, имени или типа файла
+                {
+                    hasnamecriteria = true;
+                }
+                else
+                {
+                    matchname = true;
+                }
+
+                if (textBox2.Text == "")
+                {
+                    matchtext = true;
+                }
+
+                if ((hasnamecriteria) & (file.Contains(textBox1.Text))) //проверка имени
+                {
+                    matchname = true;
+                }
+
+                filename = file.Insert(2, "\\"); //корректный путь к файлу
+                try
+                {
+                    StreamReader SR = new StreamReader(filename, Encoding.GetEncoding("windows-1251"));
+                    if (SR.ReadToEnd().Contains(textBox2.Text))
+                    {
+                        matchtext = true;
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+                if ((matchtext) & (matchname))
+                { //вывод пути к файлу, если удовлетворяет требованиям
+                    if (this.InvokeRequired)
+                    {
+                        this.Invoke((MethodInvoker)(() =>
+                        {
+                            listBox1.Items.Add(file);
+                        }
+                        ));
+                    }
+                }
+                progress = (count * 100) / files.Length;
+                t2 = DateTime.Now;
+                ts = t2 - t1;
+                time = ts.Minutes.ToString() + "мин. " + ts.Seconds.ToString() + "сек.";
+                displayresult(count, progress, file, time);
+            }
+
+        }
+
+        public void displayresult(int counttoshow, int progresstoshow, string currentfile, string timetoshow)
+        { //вывод текущей информации о поиске
+            if (this.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)(() =>
+                {
+                    label11.Text = timetoshow;
+                    progressBar1.Value = progresstoshow;
+                    label10.Text = currentfile;
+                    label12.Text = counttoshow.ToString();
+                }
+                ));
+            }
+
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            searching.Suspend();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (searching.ThreadState.ToString() == "Suspended")
+            {
+                searching.Resume();
+            }
+            searching.Abort();
+            Properties.Settings.Default.name = textBox1.Text;
+            Properties.Settings.Default.type = textBox4.Text;
+            Properties.Settings.Default.directory = textBox3.Text;
+            Properties.Settings.Default.text = textBox2.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Properties.Settings.Default.name;
+            textBox4.Text = Properties.Settings.Default.type;
+            textBox3.Text = Properties.Settings.Default.directory;
+            directory = Properties.Settings.Default.directory;
+            textBox2.Text = Properties.Settings.Default.text;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (searching.ThreadState.ToString()=="Suspended")
+            {
+                searching.Resume();
+            }
+            searching.Abort();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button4.Enabled = false;
+            listBox1.Items.Clear();
+            label11.Text = "";
+            progressBar1.Value = 0;
+            label10.Text = "";
+            label12.Text = "";
+
+        }
+    }
+}
